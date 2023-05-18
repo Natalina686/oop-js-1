8822ee9b92381c3405486559ff1d29924dd64fea
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-05-18 22:40:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-// Реалізуйте наступну систему, схожу до розглянутої на вебінарі:
+"use strict";
 
-// 1) чотири класи для створення об'єктів-сутностей (це можуть бути тварини, покемони, раси і т.д. - проявіть фантазію)
 class Nutrition {
-    #calories;
-    constructor(calories) {
-        this.#calories = calories;
+   
+    constructor(calories, dessert, snack) {
+        this.calories = calories;
+        this.dessert = dessert;
+        this.snack = snack;
     }
-    get calories() {
-        return this.#calories;
-    }
-    set calories(newCalories) {
-        this.#calories = newCalories;
-    }
+getCalories() {
+    console.log("Huge number of calories", this.calories);
 }
 
-// const summarise = new Nutrition("forget about calories");
-// console.log(summarise);
-// console.log(summarise.calories);
+    sayYammy() {
+        console.log("Mmm", this.dessert);
+    }
+}
+const cake = new Nutrition(400, "Raffaello", "to coffe");
+cake.sayYammy();
+const wisky = new Nutrition(1000, "Wisky 5 star", "to good mood");
+wisky.getCalories();
 
 
+class Supplement extends Nutrition {
+    #spicy;
+constructor (calories, dessert, snack, salty, spicy) {
+    super(calories, dessert, snack);
+    this.salty = salty;
+    this.#spicy = spicy;
+}
+getSalty() {
+    return this.salty;
+}
+
+getSpicy () {
+    return this.#spicy;
+}
+}
 
 class Food extends Nutrition {
     #fatty;
-constructor (calories, vegan, fatty, healthy) {
-    super(calories);
+constructor (calories, dessert, snack, vegan, fatty, healthy) {
+    super(calories, dessert, snack);
     this.vegan = vegan;
     this.#fatty = fatty;
     this.healthy = healthy;
@@ -37,16 +54,23 @@ getFatty() {
 getVegan () {
     return this.vegan;
 }
+tasty() {
+    super.sayYammy();
+    console.log("delicious");
+}
+
 }
 
 
 const food = new Food("soya", "chips", "vegetables")
-console.log(food);
-const bestFood = new Food("kokos-milk", "nagets", "fruits")
-console.log(bestFood);
-const deliciousFood = new Food("tophu", "steak", "fish")
-console.log(deliciousFood);
-console.log(bestFood.getFatty());
+// console.log(food);
+// const bestFood = new Food("kokos-milk", "nagets", "fruits")
+// // console.log(bestFood);
+// // const deliciousFood = new Food("tophu", "steak", "fish")
+// // console.log(deliciousFood);
+// console.log(bestFood.getFatty());
+const fatty = new Food("cheese")
+food.tasty();
 // const summarise = new Nutrition({ calories: "forget about calories", vegan: [], fatty: [], healthy: [] } );
 // console.log(summarise);
 // console.log(summarise.calories);
@@ -71,82 +95,42 @@ class Drink extends Nutrition {
         return this.sour;
     }
 }
-const drink = new Drink("shweps", "cola", "limonchello")
-console.log(drink);
-const drinkOfMine = new Drink("shweps", "fresh", "lime-juice")
-console.log(drinkOfMine);
-const hotDrink = new Drink("B-52", "moccachino", "lemonTea")
-console.log(hotDrink);
+const drink = new Drink("200", "shweps", "cola", "limonchello")
+// console.log(drink);
+// const drinkOfMine = new Drink("shweps", "fresh", "lime-juice")
+// console.log(drinkOfMine);
+// const hotDrink = new Drink("B-52", "moccachino", "lemonTea")
+// console.log(hotDrink);
 console.log(drink.getSweet());
+drink.getCalories();
+// class Pleasure {
+//     #deadly;
+// constructor (short, long, deadly) {
+//     this.short = short;
+//     this.long = long;
+//     this.#deadly = deadly;
+// }
+// getShort() {
+//     return this.short;
+// }
+// getLong() {
+//     return this.long;
+// }
+// getDeadly () {
+//     return this.#deadly;
+// }
 
-class Pleasure {
-    #deadly;
-constructor (short, long, deadly) {
-    this.short = short;
-    this.long = long;
-    this.#deadly = deadly;
-}
-getShort() {
-    return this.short;
-}
-getLong() {
-    return this.long;
-}
-getDeadly () {
-    return this.#deadly;
-}
-
-}
-const pleasure = new Pleasure("maffin", "vacation", "drugs")
-console.log(drink);
-const pleasureOfMine = new Pleasure("chokolade", "sea-vacation", "music")
-console.log(pleasureOfMine);
-const othersPleasure = new Pleasure("ice-cream", "dance", "smoking")
-console.log(othersPleasure);
-console.log(othersPleasure.getDeadly());
-
-
-
-class Work {
-    #boring;
-constructor (enjoyable, hard, boring) {
-    this.enjoyable = enjoyable;
-    this.hard = hard;
-    this.#boring = boring;
-}
-getEnjoyable() {
-    return this.enjoyable;
-}
-getHard() {
-    return this.hard;
-}
-getBoring () {
-    return this.#boring;
-}
-get boring() {
-    return this.#boring;
-}
-set boring(newBoring) {
-this.#boring = newBoring;
-}
-}
+// }
+// const pleasure = new Pleasure("maffin", "vacation", "drugs")
+// console.log(drink);
+// const pleasureOfMine = new Pleasure("chokolade", "sea-vacation", "music")
+// console.log(pleasureOfMine);
+// const othersPleasure = new Pleasure("ice-cream", "dance", "smoking")
+// console.log(othersPleasure);
+// console.log(othersPleasure.getDeadly());
 
 
-const work = new Work("light", "choping", "cleaning");
-console.log(work);
-const workOfMine = new Work("coding", "cooking", "washing");
-console.log(workOfMine);
-const anotherWork = new Work("crafting", "ironing", "singing");
-console.log(anotherWork);
-console.log(work.getBoring());
-work.boring = "collecting";
-console.log(work.boring);
-// 2) у кожного класу має бути мінімум 3 властивості та мінімум 3 методи(але можна й більше)
+const supplement = new Supplement(240, "chooros", "fish", "sea-salt", "pepper");
+console.log(supplement);
 
-// 3) у кожного класу має бути своя унікальна властивість
 
-// 4) у кожного класу має бути приватна властивість
-
-// 4) у двох класів має бути спільний предок та спільний метод характерний тільки для них
-
-// 5) у всіх чотирьох класів має бути один (крім проміжних) клас-предок
